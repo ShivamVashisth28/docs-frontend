610b73b57302aafa9a929e2599b42012430c8778
Shivam Vashisth <vashisth11shivam@gmail.com>
2024-12-26 16:54:29 +0530

    doc preview on home page

--- src/components/dashboard/DocumentCard.jsx
+++ src/components/dashboard/DocumentCard.jsx
@@ -1,7 +1,8 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { toast } from 'react-toastify'
+import  {QuillDeltaToHtmlConverter}  from 'quill-delta-to-html'
+
 
 function DocumentCard({
     role,
@@ -10,6 +11,7 @@ function DocumentCard({
 
   const navigate = useNavigate()
   const [title, setTitle] = useState("")
+  const [htmlContent, setHtmlContent] = useState('')
 
 
   const getTitle = async ()=>{
@@ -23,15 +25,34 @@ function DocumentCard({
       setTitle(data['message'])
     }
   }
+
+  const getContent = async () => {
+    const response = await axios.get(`http://localhost:5000/document/content?documentId=${documentId}`)
+    const data = await response.data
+   
+    if(data['status'] === 'success'){
+      console.log(JSON.parse(data['content']))
+      const delta = JSON.parse(data['content'])
+      console.log(delta.ops)
+
+      const convertor = new QuillDeltaToHtmlConverter(delta.ops, {});
+      const html = convertor.convert()
+      setHtmlContent(html)
+    }
+  }
+
   useEffect(()=>{
     getTitle()
+    getContent()
   })
+
+
   return (
     <div 
         className='m-2 h-[14rem]  flex flex-col bg-blue-300 shadow-xl rounded-md hover:shadow-md cursor-pointer'
         onClick={()=>navigate(`/document/${documentId}`)}
       >
-        <div className='h-[80%] bg-gray-100 rounded-t-md'></div>
+        <div className='h-[80%] bg-gray-100 rounded-t-md p-4 text-sm italic overflow-hidden ' dangerouslySetInnerHTML={{__html: htmlContent}}></div>
         <div className='h-[30%] flex flex-col pl-2 pt-1'>
             {/* <div>Title : <span className='italic'>{title}</span></div>
             <div>Role : <span className='italic'>{role}</span></div> */}
