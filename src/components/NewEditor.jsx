70e5df11c65356d605ff12d731d3a17f0e5459d9
Shivam Vashisth <vashisth11shivam@gmail.com>
2024-12-27 12:06:35 +0530

    viewer access log

--- src/components/NewEditor.jsx
+++ src/components/NewEditor.jsx
@@ -66,6 +66,7 @@ export default function NewEditor({setConnectedUsers, userType}) {
         quill.setContents()
       }
       if(userType && userType !== 'viewer'){
+        console.log(userType)
         quill.enable()
       }
     })
